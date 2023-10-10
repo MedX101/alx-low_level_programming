f4436c904f28808a952da7d72577964df5d254e2
MedX101 <dgkhorombi@gmail.com>
2023-10-10 02:30:41 -0700

    Add holberton.h header file

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int _abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif /* HOLBERTON_H */
+
